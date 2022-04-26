2c1c5b2645d4f5a4b2283fdce8496b34cb77c0b3
yaron cohen <yaroncbtv@outlook.com>
2022-04-26 16:42:03 +0300

    add build

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-const baseURL = "https://localhost:44319";
+//const baseURL = "https://localhost:44319";
+const baseURL = "https://restaurantbackend220220426133039.azurewebsites.net";
 
 
 export const addNewUser = async (newUser) => {
